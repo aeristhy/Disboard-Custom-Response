f50e7a62fe4735c1c3f67c642b11360bb1337d79
aeristhy <aerisdoescodes@gmil.com>
2021-06-16 14:59:13 +0800

    a custom response

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
-# Disboard-Custom-Response
-Just a simple DISBOARD Custom Response.
+const Discord = require('discord.js');
+const config = require('./config.json')
+const client = new Discord.Client();
 
-# CopyPaste
-```js
+client.on('ready', async () => {
+    console.log('Ready!')
+})
+
+client.on('message', async message => {
     if (message.embeds.length && message.author.username == 'DISBOARD' && message.embeds[ 0 ].description.indexOf('Bump done') > -1) {
         message.delete();
         return message.channel.send('Thank you for bumping! Make sure to leave us a review (link your DISBOARD server)');
@@ -12,4 +16,6 @@ Just a simple DISBOARD Custom Response.
             return message.channel.send('Bump is not ready yet');
         }
     }
-```
+});
+
+client.login(config.token);
